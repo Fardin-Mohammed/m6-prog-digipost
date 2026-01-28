a378d781c7f91315e70bc2f5f8361de0bea1fe08
Fardin <39059@ma-web.nl>
2026-01-28 14:16:33 +0100

    probleem gefixt van connectie

--- html/public/source/views/db.php
+++ html/public/source/views/db.php
@@ -1,13 +1,11 @@
 <?php
 $host = "mysql";
-$dbname = "m6prog_digipost";
-$user = "m6prog_digipost";
+$dbname = "m6prog";
+$user = "m6proggebruiker";
 $pass = "ikben89die@guy";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-
